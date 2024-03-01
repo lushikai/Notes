7f8ec9ecc094dc2e895d1a514c1804c26ee1c74a
lushikai <2807055803@qq.com>
2024-03-01 17:33:23 +0800

    update

--- algorithm/data_structure/string/string_using_array.cpp
+++ algorithm/data_structure/string/string_using_array.cpp
@@ -16,18 +16,17 @@ typedef struct {
 //     int length;
 // }HString;
 
-
 bool SubString(SString &Sub, SString S, int pos, int len) {
     if (pos + len - 1 > S.length) {return false;}
     for (int i = pos; i < pos + len; i++) {
-        Sub.ch[i-pos] = S.ch[i];
+        Sub.ch[i-pos+1] = S.ch[i];
     }
     Sub.length = len;
     return true;
 }
 
 int StrCompare(SString S, SString T) {
-    for (int i = 0; i <= S.length && i <= T.length; i++) {
+    for (int i = 1; i <= S.length && i <= T.length; i++) {
         if (S.ch[i] != T.ch[i]) {
             return S.ch[i] - T.ch[i];
         }
@@ -37,32 +36,36 @@ int StrCompare(SString S, SString T) {
 
 //O(mn)
 int Index(SString S, SString T) {
-    int i = 0, j = 0;
-    while(i < S.length && j < T.length) {
-        if (S.ch[i] == T.ch[j]) {
+    int i = 1, j = 1;
+    while(i <= S.length && j <= T.length) {
+        if (S.ch[i-1] == T.ch[j-1]) {
             ++i;
             ++j;
         }
         else {
-            i = i - j + 1;
-            j = 0;
+            i = i - j + 2;
+            j = 1;
         }
     }
-    if (j >= T.length) {
+    if (j > T.length) {
         return i - T.length;
     }
-    else {return -1;}
+    else {return 0;}
 }
 
+/**
+ * next[1] -> 0
+ * 
+*/
 void GetNext() {
     
 }
 
 //O(m+n)
 int KMP(SString S, SString T, int next[]) {
-    int i = 0, j = 0;
-    while(i < S.length && j < T.length) {
-        if (j == -1 || S.ch[i] == T.ch[j]) {
+    int i = 1, j = 1;
+    while(i <= S.length && j <= T.length) {
+        if (j == 0 || S.ch[i] == T.ch[j]) {
             ++i;
             ++j;
         }
@@ -70,10 +73,10 @@ int KMP(SString S, SString T, int next[]) {
             j = next[j];
         }
     }
-    if (j >= T.length) {
+    if (j > T.length) {
         return i - T.length;
     }
-    else {return -1;}
+    else {return 0;}
 }
 
 
@@ -82,8 +85,8 @@ int KMP(SString S, SString T, int next[]) {
 
 int main() {
     using namespace string_using_array;
-    SString str1 = {"ababcabcacbab", 13};
-    SString str2 = {"abcac", 5};
+    SString str1 = {"helloWorld",10};
+    SString str2 = {"World",5};
     std::cout << Index(str1, str2) << std::endl;
     // std::cout << KMP(str1, str2) << std::endl;
 
